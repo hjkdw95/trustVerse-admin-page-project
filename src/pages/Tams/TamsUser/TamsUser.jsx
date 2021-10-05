dbaf6deaea3bc7961deb7e268282f6dfd48da449
Casey <hjkdw9506@gmail.com>
2021-10-05 17:58:53 +0900

    🚧 Add : [Tams] user page markup 중

--- src/pages/Tams/TamsUser/TamsUser.jsx
+++ src/pages/Tams/TamsUser/TamsUser.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const TamsUser = props => {
-  return <div>hi</div>;
+  return (
+    <>
+      <H1>Users</H1>
+    </>
+  );
 };
 
 export default TamsUser;
+
+const H1 = styled.h1`
+  ${({ theme }) => theme.title()}
+`;
