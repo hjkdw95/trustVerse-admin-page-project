5c8e80f9037b620947ef0ac9a39ad0db95930242
Casey <hjkdw9506@gmail.com>
2021-10-06 13:28:24 +0900

    ✨ Add : table header component - 레이아웃 및 onClick effect 구현 완료

--- src/pages/Tams/TamsUser/UserTable.jsx
+++ src/pages/Tams/TamsUser/UserTable.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import TableHeader from './Table/TableHeader';
 import styled from 'styled-components';
 
 const Table = ({ info }) => {
+  const [currentID, setCurrentID] = useState();
+  const [isClicked, setClick] = useState(false);
   // const keyName = Object.keys(info[0]);
 
   const HEADERS = [
@@ -20,21 +22,36 @@ const Table = ({ info }) => {
     'PWD Edited Date',
   ];
 
+  const handleClick = () => {
+    setClick(!isClicked);
+  };
+
   return (
-    <>
-      <Header>
-        {HEADERS.map(header => (
-          <div>{header}</div>
-        ))}
-      </Header>
-    </>
+    <TableWrapper>
+      <TableHead>
+        <Header role="row">
+          {HEADERS.map((header, idx) => (
+            <TableHeader header={header} key={idx} />
+          ))}
+        </Header>
+      </TableHead>
+      <tbody></tbody>
+    </TableWrapper>
   );
 };
 
 export default Table;
 
-const Header = styled.div`
-  div {
-    text-transform: capitalize;
-  }
+const TableWrapper = styled.div`
+  background-color: #fff;
+`;
+
+const TableHead = styled.thead`
+  width: 100%;
+  margin-bottom: 1rem;
+`;
+
+const Header = styled.tr`
+  display: flex;
+  justify-content: space-between;
 `;
