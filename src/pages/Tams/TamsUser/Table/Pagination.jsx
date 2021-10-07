d26f76f1fe86ea0baa8f7e5ee2b20b252599fc58
Casey <hjkdw9506@gmail.com>
2021-10-07 10:12:59 +0900

    ✨ Add : pagination 구현 완료

--- src/pages/Tams/TamsUser/Table/Pagination.jsx
+++ src/pages/Tams/TamsUser/Table/Pagination.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import ReactPaginate from 'react-paginate';
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from 'react-icons/ai';
 import styled from 'styled-components';
 
 const Pagination = ({ info, offSet, limit, setLimit, setOffset }) => {
   const handleChange = data => {
-    // console.log(data.selected);
     let selected;
     let newOffset;
     if (data.selected === 0) {
@@ -19,26 +19,69 @@ const Pagination = ({ info, offSet, limit, setLimit, setOffset }) => {
   };
 
   return (
-    <>
-      <div>
+    <Wrapper>
+      <ListSummary>
         Showing {limit + 1} to {limit + 7} of {info.length} entries
-      </div>
-      <div>
+      </ListSummary>
+      <PageinationWrapper>
         <ReactPaginate
-          previousLabel={'previous'}
-          nextLabel={'next'}
+          previousLabel={<AiOutlineArrowLeft />}
+          nextLabel={<AiOutlineArrowRight />}
           breakLabel={'...'}
           breakClassName={'break-me'}
-          pageCount={info.length}
+          pageCount={Math.ceil(info.length / 7)}
           marginPagesDisplayed={2}
           pageRangeDisplayed={5}
           onPageChange={handleChange}
           containerClassName={'pagination'}
           activeClassName={'active'}
         />
-      </div>
-    </>
+      </PageinationWrapper>
+    </Wrapper>
   );
 };
 
 export default Pagination;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 4rem 0 3rem 0;
+`;
+
+const ListSummary = styled.div`
+  ${({ theme }) => theme.tableFont()};
+  font-weight: 700;
+`;
+
+const PageinationWrapper = styled.div`
+  .pagination {
+    display: flex;
+
+    a {
+      margin: 0 0.5rem;
+      transition: all 0.5s;
+      font-weight: 700;
+    }
+
+    li > a {
+      display: inline-block;
+      width: 3.2rem;
+      height: 3.2rem;
+      border-radius: 0.3rem;
+      font-size: 1.4rem;
+      text-align: center;
+      line-height: 3.2rem;
+      color: ${props => props.theme.black};
+      cursor: pointer;
+
+      &:hover {
+        ${({ theme }) => theme.greenBtn()}
+      }
+    }
+
+    .active > a {
+      ${({ theme }) => theme.greenBtn()}
+    }
+  }
+`;
