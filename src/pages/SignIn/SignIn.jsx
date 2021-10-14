5bbba6b3d5737d677427c1589180ea73c9ce471c
최파란별 <chlvkfksquf@gmail.com>
2021-10-14 13:01:04 +0900

    Add : 회원가입 유효성검사로직 구현

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -2,37 +2,104 @@ import { React, useState } from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
-const SignIn = props => {
+const SignIn = ({ history }) => {
   const [idValue, setIdValue] = useState('');
   const [pwValue, setPwValue] = useState('');
+  const [checkPwValue, setCheckPwValue] = useState('');
 
   const handleInputId = e => {
     setIdValue(e.target.value);
   };
+
   const handleInputPw = e => {
     setPwValue(e.target.value);
   };
+
+  const handleInputCheckPw = e => {
+    setCheckPwValue(e.target.value);
+  };
+
+  const postSignUp = () => {
+    fetch('', {
+      method: 'POST',
+      body: JSON.stringify({
+        name: idValue,
+        password: pwValue,
+      }),
+    })
+      .then(response => response.json())
+      .then(response => {
+        if (response.MESSAGE === 'SUCCESS') {
+          alert('가입 축하드립니다.');
+          return history.push('/Signin');
+        } else if (response.MESSAGE === 'DUPLICATED ADMIN NAME') {
+          alert('이미 가입된 아이디 입니다.');
+        }
+      });
+  };
+
+  const resetInfo = () => {
+    setIdValue('');
+    setPwValue('');
+    setCheckPwValue('');
+    alert('가입 정보를 다시 확인해 주세요');
+  };
+
+  const checkLogin = () => {
+    const str = /[A-Z]/;
+    const special = /[~!@#$%^&*()_+|<>?:{}]/;
+
+    const formCheck =
+      idValue.length > 7 &&
+      idValue.length < 17 &&
+      str.test(pwValue) &&
+      special.test(pwValue) &&
+      pwValue.length >= 8 &&
+      checkPwValue === pwValue;
+
+    formCheck ? postSignUp() : resetInfo();
+  };
+
   return (
     <div>
       <SingUpBox>
-        <SingUpTitle>Admin SignIn</SingUpTitle>
+        <SingUpTitle>Admin Sign In</SingUpTitle>
         <SingUpArticle>
-          <div>
-            <IdInput
-              placeholder="Name"
-              onChange={handleInputId}
-              value={idValue}
-            ></IdInput>
-          </div>
-          <div>
-            <PwInput
-              placeholder="Password"
-              type="password"
-              onChange={handleInputPw}
-              value={pwValue}
-            ></PwInput>
-          </div>
-          <SignUpBnt>SignIn</SignUpBnt>
+          <Form onsubmit="return false;">
+            <div>
+              <Caution>8자 이상 16글자 미만</Caution>
+              <IdInput
+                placeholder="Id"
+                onChange={handleInputId}
+                value={idValue}
+                minLength="8"
+                maxLength="16"
+                required
+              ></IdInput>
+            </div>
+            <div>
+              <Caution>영어, 특수문자 모두 포함 8자 이상</Caution>
+              <PwInput
+                placeholder="Password"
+                type="password"
+                onChange={handleInputPw}
+                value={pwValue}
+                minLength="8"
+                required
+              ></PwInput>
+            </div>
+            <div>
+              <PwInput
+                placeholder="Check Password"
+                type="password"
+                onChange={handleInputCheckPw}
+                value={checkPwValue}
+                minLength="8"
+                required
+              ></PwInput>
+            </div>
+            <SignUpBnt onClick={checkLogin}>Sign In</SignUpBnt>
+          </Form>
           <SignUpText>
             Need an account?<Link to="/Signin">Sign Up</Link>
           </SignUpText>
@@ -51,6 +118,8 @@ const SingUpBox = styled.div`
   margin-top: 100px;
   background-color: white;
 `;
+
+const Form = styled.form``;
 
 const SingUpTitle = styled.div`
   background-color: #a37bfd;
@@ -88,11 +157,17 @@ const SignUpBnt = styled.button`
   width: 100%;
   font-size: 16px;
   margin-bottom: 20px;
+
   &:hover {
     cursor: pointer;
   }
 `;
 
+const Caution = styled.div`
+  font-size: 10px;
+  color: red;
+  text-align: left;
+`;
 const SignUpText = styled.div`
   color: #707070;
   font-size: 20px;
