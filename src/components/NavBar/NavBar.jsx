fbd87a7c46222f14ffa87fcf04d69f150577ab11
Casey <hjkdw9506@gmail.com>
2021-10-18 10:12:48 +0900

    conflict 해결

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -18,13 +18,58 @@ const NavBar = () => {
     setActiveIndex(null);
   };
 
-const NavBar = props => {
   return (
-    <>
-      <AccordianMenu />
-      <StatusBar />
-    </>
+    <Nav>
+      <TitleWrapper>
+        <Title onClick={handleClick}>DigiFinance</Title>
+      </TitleWrapper>
+      <Ul>
+        {MENU_LIST.map((item, idx) => {
+          const active = idx === activeIndex ? 'active' : '';
+          return (
+            <ListItem
+              title={item.title}
+              idx={idx}
+              key={idx}
+              list={item.list}
+              active={active}
+              activeIndex={activeIndex}
+              setActiveIndex={setActiveIndex}
+            />
+          );
+        })}
+      </Ul>
+    </Nav>
   );
 };
 
 export default NavBar;
+
+const Nav = styled.nav`
+  width: 15%;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+  background-color: #0f1a34;
+  color: #fff;
+  overflow-x: hidden;
+  z-index: 1000;
+`;
+
+const TitleWrapper = styled.div`
+  padding: 2.5rem;
+  background-color: #182444;
+`;
+
+const Title = styled.h1`
+  font-size: 2.5rem;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const Ul = styled.ul`
+  background-color: #0f1a34;
+  overflow-y: auto;
+`;
